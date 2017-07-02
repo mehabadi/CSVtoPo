673c337edfdbd8adfedb0acc65d810e4069ebc8c
mmahabadi <mmahabadi@M-MAHABADI-12>
2017-07-02 12:22:49 +0430

    Add project files.

---
+++ CsvToPo/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CsvToPo.Models;
+
+namespace CsvToPo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public string CsvToPo()
+        {
+            //using (var db = new developaEntities())
+            //{
+            //    db.
+            //}
+            return "";
+        }
+    }
+}
